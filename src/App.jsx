f486dd10dc882630051eae86d22fdbe769ee0b97
dessy1287 <117296641+dessy1287@users.noreply.github.com>
2022-11-10 15:36:46 -0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App =() => {
+  return (
+    <div className="App">
+      <hi className="text-3x1 front-bold underline">
+        hello world!
+          </hi>
+        </div>        
+  )
+}
+
+export default App
